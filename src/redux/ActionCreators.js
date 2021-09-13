2e0cc3cf809cc01201ea8a88fa3905c03a939a46
Andy Chou <andychchou@gmail.com>
2021-09-12 18:11:59 -0700

    Week 5 Redux Thunk

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,4 +1,5 @@
 import * as ActionTypes from './ActionTypes';
+import { CAMPSITES } from '../shared/campsites';
 
 export const addComment = (campsiteId, rating, author, text) => ({
     type: ActionTypes.ADD_COMMENT,
@@ -8,4 +9,29 @@ export const addComment = (campsiteId, rating, author, text) => ({
         author: author,
         text: text
     }
-})
+});
+
+//nested arrow functions enabled by redux thunk
+export const fetchCampsites = () => dispatch => {
+
+    dispatch(campsitesLoading());
+
+    setTimeout(() => {
+        dispatch(addCampsites(CAMPSITES));
+    }, 2000);
+};
+
+//not using redux thunk, just returns an action object
+export const campsitesLoading = () => ({
+    type: ActionTypes.CAMPSITES_LOADING
+});
+
+export const campsitesFailed = errMess => ({
+    type: ActionTypes.CAMPSITES_FAILED,
+    payload: errMess
+});
+
+export const addCampsites = campsites => ({
+    type: ActionTypes.ADD_CAMPSITES,
+    payload: campsites
+});
